d3209047caba265a3942e91e4eda347bd737c877
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2026-08-19 19:04:56 +0100

    Remove redundant dist folder, crept into original

--- packages/service-framework/src/configuration.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ServiceProfile, ServiceRegistration } from "./contracts.js";
-export declare function createServiceProfile(name: string, services: readonly ServiceRegistration[]): ServiceProfile;
-//# sourceMappingURL=configuration.d.ts.map
